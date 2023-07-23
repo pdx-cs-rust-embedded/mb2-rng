fede5fd066aec35ed00e02044b32ef5504a50ec2
Bart Massey <bart.massey@gmail.com>
2023-07-23 00:42:58 -0700

    added LED die value display

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,11 @@
 #![no_main]
 
 use cortex_m_rt::entry;
-#[rustfmt::skip]
-use microbit::{Board, hal::Rng as HwRng};
+use microbit::{
+    Board,
+    display::blocking::Display,
+    hal::{Rng as HwRng, timer::Timer},
+};
 use nanorand::{Rng, SeedableRng};
 use panic_rtt_target as _;
 use rtt_target::{rprintln, rtt_init_print};
@@ -31,20 +34,80 @@ fn reseed(sw_rng: &mut SwRng, hw_rng: &mut HwRng) {
     sw_rng.reseed(seed);
 }
 
+static D6_0: [[u8; 5]; 5] = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+];
+
+static D6_1: [[u8; 5]; 5] = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+];
+
+static D6_2: [[u8; 5]; 5] = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0],
+    [0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+];
+
+static D6_3: [[u8; 5]; 5] = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0],
+    [0, 0, 1, 0, 0],
+    [0, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+];
+
+static D6_4: [[u8; 5]; 5] = [
+    [0, 0, 0, 0, 0],
+    [0, 1, 0, 1, 0],
+    [0, 0, 0, 0, 0],
+    [0, 1, 0, 1, 0],
+    [0, 0, 0, 0, 0],
+];
+
+static D6_5: [[u8; 5]; 5] = [
+    [0, 0, 0, 0, 0],
+    [0, 1, 0, 1, 0],
+    [0, 0, 1, 0, 0],
+    [0, 1, 0, 1, 0],
+    [0, 0, 0, 0, 0],
+];
+
+static D6_6: [[u8; 5]; 5] = [
+    [0, 0, 0, 0, 0],
+    [0, 1, 0, 1, 0],
+    [0, 1, 0, 1, 0],
+    [0, 1, 0, 1, 0],
+    [0, 0, 0, 0, 0],
+];
+
+static D6: [[[u8; 5]; 5]; 7] = [ D6_0, D6_1, D6_2, D6_3, D6_4, D6_5, D6_6 ];
+
 #[entry]
 fn init() -> ! {
     rtt_init_print!();
 
     let board = Board::take().unwrap();
     let mut hw_rng = HwRng::new(board.RNG);
+    let mut frame_timer = Timer::new(board.TIMER0);
+    let mut display = Display::new(board.display_pins);
+
     let mut rng = SwRng::new_seed(1);
     reseed(&mut rng, &mut hw_rng);
 
-    for _ in 0..10 {
-        let r: u8 = rng.generate_range(1..7);
-        rprintln!("{}", r);
-    }
     loop {
-        continue;
+        let r: usize = rng.generate_range(1..7);
+        rprintln!("{}", r);
+        display.show(&mut frame_timer, D6[r], 700);
+        display.show(&mut frame_timer, D6[0], 300);
     }
 }
